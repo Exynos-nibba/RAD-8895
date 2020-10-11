6bfa6c105d1f3a7ca53898c51a783e7a1032b515
Live0verfl0w <vsht700@gmail.com>
2020-10-11 15:21:13 +0400

    RAD-ify

---
+++ RAD/ak3-s8/anykernel.sh
@@ -0,0 +1,84 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=// RAD Kernel // #StayRAD // 
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=dreamlte
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/11120000.ufs/by-name/BOOT;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+ui_print "Cleaning old Kernel leftovers...";
+mount -o remount,rw /system;
+rm -f $ramdisk/rz/scripts/40perf;
+rm -f $ramdisk/rz/scripts/90userinit;
+rm -f $ramdisk/init.spectrum.rc;
+rm -f $ramdisk/init.spectrum.sh;
+rm -f $ramdisk/sbin/rz_kernel.sh;
+rm -f /system/bin/sysinit_cm;
+rm -f /system/etc/init.d/30zram;
+rm -f /system/etc/init.d/40perf;
+rm -f /system/etc/init.d/90userinit;
+rm -rf /data/rz_system;
+rm -rf /system/rz_system;
+
+ui_print "Initializing init.d support...";
+mkdir /system/etc/init.d;
+chmod 755 /system/etc/init.d;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+backup_file init.rc;
+replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+# init.tuna.rc
+backup_file init.tuna.rc;
+insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+append_file init.tuna.rc "bootscript" init.tuna;
+
+# fstab.tuna
+backup_file fstab.tuna;
+patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+append_file fstab.tuna "usbdisk" fstab;
+
+# end ramdisk changes
+
+write_boot;
+
+ui_print "Done! - RAD-ified ur device!";
+
+## end install
+
---
+++ build.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+echo "------------------------------------------------------"
+echo "---             RAD-KERNEL-BUILD-SCRIPT            ---"
+echo "------------------------------------------------------"
+
+DATE=$(date +'%Y%m%d')
+
+echo "Make sure you have specified your device! [export DEVICE=<N8/S8/S8p>]"
+echo "Make sure you have specified your Kernel build version! [export VERSION=<build_number>]"
+
+if [ "${DEVICE}" == "S8" ]; then
+		export DEFCONFIG=dreamlte;
+		export AK3_PATH=ak3-s8;
+	elif [ "${DEVICE}" == "S8p" ]; then
+		export DEFCONFIG=dream2lte;
+		export AK3_PATH=ak3-s8+;
+	elif [ "${DEVICE}" == "N8" ]; then
+		export DEFCONFIG=greatlte;
+		export AK3_PATH=ak3-n8;
+	fi;
+
+export LOCALVERSION=-RAD-${VERSION}-${DATE}-AOSP
+
+echo "...................................."
+echo "...................................."
+echo ".. ""SETTING TOOLCHAIN AND ARCH"" .."
+echo "...................................."
+echo "...................................."
+export ARCH=arm64
+export PATH="$(pwd)/clang/bin/:$(pwd)/toolchain/bin:${PATH}"
+export CROSS_COMPILE=$(pwd)/toolchain/bin/aarch64-linux-gnu-
+
+echo ....................................
+echo ....................................
+echo .. ""CREATING/REMOVING OUT DIR""  ..
+echo ....................................
+echo ....................................
+rm -rf out
+mkdir -p out
+
+echo ....................................
+echo ....................................
+echo ........""CLEANING SOURCE"".........
+echo ....................................
+echo ....................................
+make O=out clean && make O=out mrproper
+
+echo ....................................
+echo ....................................
+echo ...""BUILDING KERNEL "".............
+echo ....................................
+echo ....................................
+make O=out exynos8895-${DEFCONFIG}_defconfig && make O=out CC=clang -j4
+
+echo ""making zip""
+if [ -e $(pwd)/out/arch/arm64/boot/Image ]; then
+	echo -e "Making AK3 ZIP"
+	rm -rf $(pwd)/RAD/${AK3_PATH}/Image
+ 	
+	rm -rf $(pwd)/RAD/${AK3_PATH}/kernel.zip
+
+	echo -e "copying zimage to ak3 folder"
+	cp $(pwd)/out/arch/arm64/boot/Image $(pwd)/RAD/${AK3_PATH}
+ 	
+	echo ""
+
+	echo -e "zipping up ak3"
+	cd ~/kernel/ausp/RAD/${AK3_PATH}
+	zip -r9 kernel.zip * -x README.md kernel.zip/
+	mkdir ~/kernel/ausp/RAD/Releases/${VERSION}
+	mv kernel.zip ~/kernel/ausp/RAD/Releases/${VERSION}/RAD-${VERSION}-${DEFCONFIG}-AOSP.zip
+ 	
+	echo "Done!"
+
+	echo ""
+else
+	echo Kernel didnt build successfully!
+	echo No zIMAGE in out dir
+	echo Exiting!
+fi;
+
+exit 0
+
